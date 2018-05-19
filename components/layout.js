cd798beec9aca3d9bed8a8e87c0a60c37d235915
rappad <wasuwat.se@gmail.com>
2018-05-19 23:32:50 +0700

    add babel pluing to fixed styled comp

--- components/layout.js
+++ components/layout.js
@@ -1,6 +1,19 @@
 import { Fragment } from "react";
 import Header from "./header";
-import styled from "styled-components";
+import styled, { injectGlobal } from "styled-components";
+
+injectGlobal`
+@import url('https://fonts.googleapis.com/css?family=Montserrat');
+  html {
+    height: 100%;
+    width: 100%;
+  }
+  body{
+    margin:0;
+    padding:0;
+    font-family: 'Montserrat', sans-serif;
+  }
+`;
 
 const ContentWrapper = styled.div`
   margin: 0;
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,6 @@
 import React from "react";
 import Link from "next/link";
 import Layout from "../components/layout";
-import styled, { injectGlobal } from "styled-components";
-
-injectGlobal`
-@import url('https://fonts.googleapis.com/css?family=Montserrat');
-  html {
-    height: 100%;
-    width: 100%;
-  }
-  body{
-    margin:0;
-    padding:0;
-    font-family: 'Montserrat', sans-serif;
-  }
-`;
 
 export default () => (
   <Layout>
