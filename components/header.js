ebe1a55b3db03aa7904ed68c3e931d08f581fc71
rappad <wasuwat.se@gmail.com>
2018-05-20 00:03:52 +0700

    'test-deploy'

--- components/header.js
+++ components/header.js
@@ -20,13 +20,13 @@ const HeaderWrapper = styled.div`
 
 const Header = () => (
   <HeaderWrapper>
-    <Link prefetch href="/" as={process.env.BACKEND_URL + "/"}>
+    <Link prefetch href="/">
       <Title>Home</Title>
     </Link>
     <Link prefetch href="/about">
       <Title>About</Title>
     </Link>
-    <Link prefetch href="/contact" as={process.env.BACKEND_URL + "/contact"}>
+    <Link prefetch href="/contact">
       <Title>Contact</Title>
     </Link>
   </HeaderWrapper>
