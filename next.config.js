f1211d185bc8c3b49aed69ea4ac47d6e07a3eb4b
was.li <wasuwat.se@gmail.com>
2018-08-07 14:17:53 +0700

    test-deploy

--- next.config.js
+++ next.config.js
@@ -5,5 +5,14 @@ module.exports = {
       '/about': { page: '/about' },
       '/contact': { page: '/contact' }
     }
+  },
+  webpack: config => {
+    config.module.rules = config.module.rules.map(rule => {
+      if (rule.loader === 'babel-loader') {
+        rule.options.cacheDirectory = false
+      }
+      return rule
+    })
+    return config
   }
 }
